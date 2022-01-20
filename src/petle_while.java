f5b8c75e44cf739c28970bb72053c34111a7617f
Jacek <jacek.kuzniarr@wp.pl>
2022-01-20 17:03:16 +0100

    com,m

--- src/petle_while.java
+++ src/petle_while.java
@@ -1,70 +1,49 @@
-public class petle_while {
+import java.util.Scanner;
 
-        public static void main(String[] args) {
+public class Main {
 
-            /* cw.1 pętle while */
+    public static void main(String[] args) {
 
-        /* zad 1 - Napisać za pomocą pętli do-while kod który będzie iterował od liczby 1000 w dół do 0.
-          Jeśli liczba będzie podzielna przez 2 ostatnie cyfry waszego indeksu (podać nr indesu z uczelni)
-          to dać komunikat "liczba XX podzielna przez YY" (XX-liczba podzielna YY-podzielnik)
-        */
-/*
-        {
-    int i = 1000;
-        i = 1000;
-        System.out.println("\n");
-        do {
-            System.out.print("\nLiczba: " + i);
-            if (i % 61 == 0) {
-                System.out.println(" jest podzielna przez 61");
-            }
-        } while (--i > 0);
-    }
-        /* zad 2
-        a) stworzyć tablicę typu int wielkosci 5, przypisac jej dowolne wartości,
-          przeiterować tablicę po elementach i wyswietlić, użyć pętli while
-        b) jak wyżej tylko dla tablicy typu double[],
-        c) jak wyżej dla tablicy String[], przeiterować i wyświetlić od ostatniego elementu
-        */
-/* a)
-        int[] tab1 = {3, 8, 25, 73, 12};
-        int i = 0;
-        while (i < 5) {
-            System.out.println("i " + i + ": " + tab1[i]);
-            ++i;
-        }*/
-
-        /* b)
-        {
-            double[] tab2 = {1.15, 5.95, 10.25, 14.154, 55.874};
-            int j = 0;
-            while (j < 5) {
-                System.out.println("i " + j + ": " + tab2[j]);
-                ++j;
-            }*/
-
-            /* c)
-            {
-                String[] tab3 = {"Sklep", "Piwo", "Spacer", "Dom", "Sen"};
-                int j = 0;
-                while (j < 5) {
-                    System.out.println("i " + j + ": " + tab3[j]);
-                    ++j;
-                }*/
-
-
-        /* zad.3
-        a) sprawdzić jak zachowuje się pętla while gdy w jej warunku damy true --> while(true)
-        b) a jak gdy damy false --> while(false)
-        */
-      /*  while (true) {
-            System.out.println("SPAM");
-        }*/
-
-       /* while (false) {
-            System.out.println("NIC");
-        }*/
-
+        /* ćw 1 enum-y */
+        Scanner scan = new Scanner(System.in);
+        enum LiczbyEnum {
+            JEDEN, DWA, TRZY, CZTERY, PIĘĆ, SZEŚĆ;
         }
-    }
+        enum StatusEnum {
+            KONTYNUUJEMY, KONIEC;
+        }
+        while (true) {
+            System.out.println("Podaj liczbę:");
+            int number = scan.nextInt();
+            if (number == 0) {
+                break;
+            }
+            switch (scan.nextInt()) {
+                case 0:
+                    System.out.println("KONIEC");
+                    break;
+                default:
+                    System.out.println("KONTYNUUJEMY");
+                    break;
+                case 1:
+                    System.out.println("Jeden");
+                    break;
+                case 2:
+                    System.out.println("Dwa");
+                    break;
+                case 3:
+                    System.out.println("Trzy");
+                    break;
+                case 4:
+                    System.out.println("Cztery");
+                    break;
+                case 5:
+                    System.out.println("Pięć");
+                    break;
+                case 6:
+                    System.out.println("Sześć");
+                    break;
 
+
+            }
+        }
